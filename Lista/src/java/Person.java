b791358e5a1510ebc5a6fd1aab45bf226e777b74
Peragorn <damian16172@gmail.com>
2014-10-14 13:24:22 +0200

    PAWWW

---
+++ Lista/src/java/Person.java
@@ -0,0 +1,17 @@
+
+import java.util.LinkedList;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Damian
+ */
+class Person {
+    String imie;
+    String nazwisko;
+    int rok_ur;
+}
